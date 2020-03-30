265a0350901448bfeabc9e3d7f38e383a47e4032
Ben Gardiner <bennygards@gmail.com>
2020-03-31 08:29:32 +1100

    Nested loops work week 5

---
+++ Nested repition/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace selectiondemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+       
+        //Task 1
+         /* 
+                 
+                    string name = "x";
+                    
+                     for (int counter = 0; counter <10; counter++) 
+                    {
+
+                        for (int counter2 = counter; counter2 < 5; counter2++)
+                        {
+                            System.Console.Write(name);
+                        }
+                    
+                        System.Console.WriteLine();
+                    }
+            */
+             
+        //task2
+        
+         /*    
+                    string name = "x";
+             
+                    
+                    Console.Write("please enter a number between 1 and 5\n");
+
+                    int userCounter = Int32.Parse(Console.ReadLine());
+                   
+                    for (int counter = userCounter; counter > 0; counter--){
+
+                        for (int counter2 = counter; counter2 > 0; counter2--)
+                        {
+                            System.Console.Write(name);
+                        }
+                    
+                        System.Console.WriteLine();
+                    }
+         */
+                
+             
+                    
+                    
+                        string name = "x";
+                        int counter = 1;
+                   
+                    
+
+                        for (int counter2 = counter; counter2 < 5 ; counter2++){
+                        {
+                            System.Console.Write(name);
+                        }
+                    
+                        System.Console.WriteLine(name);
+                    }
+                   
+
+
+        }
+    }
+}
